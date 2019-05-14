d8dde130dc7015286400abb68c33167e871f1641
DTI-Lakshmi <38867127+DTI-Lakshmi@users.noreply.github.com>
2019-05-14 21:17:07 +0530

    Update MainActivity.java

--- app/src/main/java/omgbuzz/devobl/com/testingproject/MainActivity.java
+++ app/src/main/java/omgbuzz/devobl/com/testingproject/MainActivity.java
@@ -50,9 +50,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.draging:
                 startActivity(new Intent(MainActivity.this, ViewDragingActivity.class));
-
-
-                
                 break;
             case R.id.colorpicker:
                 startActivity(new Intent(MainActivity.this, ColorPickerActivity.class));
